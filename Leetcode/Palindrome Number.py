ea1e63d2d1713c8e747210e417a4d7f82312f3a6
Iahia <iahiaimon555@gmail.com>
2026-09-19 14:47:37 +0600

    Movie ticket booking Calculator

---
+++ Leetcode/Palindrome Number.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x = int(input("Enter number"))
+        if x == reversed.x:
+            print("True")
+        else:
+            print("false")
